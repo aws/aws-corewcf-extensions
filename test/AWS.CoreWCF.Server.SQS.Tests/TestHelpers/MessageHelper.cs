aac0a49a45914d7a507a39f82f745ee2bd7b8915
Jon Louie <louiejon@amazon.com>
2022-09-27 19:06:26 -0700

    Add test helper classes and a test service

---
+++ test/AWS.CoreWCF.Server.SQS.Tests/TestHelpers/MessageHelper.cs
@@ -0,0 +1,37 @@
+﻿using AWS.CoreWCF.Server.Common;
+
+namespace AWS.CoreWCF.Server.SQS.Tests.TestHelpers;
+
+public class MessageHelper
+{
+    private const string TestMessageTemplate = @"<s:Envelope xmlns:s=""http://www.w3.org/2003/05/soap-envelope"" xmlns:a=""http://www.w3.org/2005/08/addressing"">
+    <s:Header>
+        <a:Action s:mustUnderstand=""1"">http://tempuri.org/{0}/{1}</a:Action>
+        <VsDebuggerCausalityData xmlns=""http://schemas.microsoft.com/vstudio/diagnostics/servicemodelsink"">
+            uIDPo4IM/u5L8tFGizKmemV7kmEAAAAAcStFY6F8ukKPmzryw1DTrsJZH/CtAKlEjIDYla6EnwYACQAA
+        </VsDebuggerCausalityData>
+    </s:Header>
+    <s:Body>
+        <{1} xmlns=""http://tempuri.org/"">
+            <name>TestMessage</name>
+        </{1}>
+    </s:Body>
+</s:Envelope>";
+    
+
+    public static async Task SendMessageToQueueAsync(string iServiceName, string actionName)
+    {
+        var client = SdkClientHelper.GetSqsClientInstance();
+        await client.SendMessageAsync(EnvironmentCollectionFixture.GetTestQueueUrl(), GetTestMessage(iServiceName, actionName));
+    }
+
+    private static string GetTestMessage(string iServiceName, string actionName)
+    {
+        return string.Format(TestMessageTemplate, iServiceName, actionName);
+    }
+
+    private static Stream GetTestMessageAsStream(string iServiceName, string actionName)
+    {
+        return GetTestMessage(iServiceName, actionName).ToStream();
+    }
+}   
---
+++ test/AWS.CoreWCF.Server.SQS.Tests/TestService/ServiceContract/LoggingService.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using CoreWCF;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AWS.CoreWCF.Server.SQS.Tests.TestService
+{
+    internal static class Constants
+    {
+        public const string NS = "http://tempuri.org/";
+        public const string LOGGINGSERVICE_NAME = nameof(ILoggingService);
+        public const string OPERATION_BASE = NS + LOGGINGSERVICE_NAME + "/";
+    }
+
+    [ServiceContract(Namespace = Constants.NS, Name = Constants.LOGGINGSERVICE_NAME)]
+    //[ServiceContract]
+    public interface ILoggingService
+    {
+        [OperationContract(
+            Name = "LogMessage",
+            Action = Constants.OPERATION_BASE + "LogMessage",
+            IsOneWay = true)]
+        //[OperationContract(IsOneWay = true)]
+        public void LogMessage(string toLog);
+    }
+
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class LoggingService : ILoggingService
+    {
+        private IServiceProvider _services;
+        public ManualResetEventSlim ManualResetEvent { get; }
+
+        public LoggingService(IServiceProvider services)
+        {
+            _services = services;
+            ManualResetEvent = new ManualResetEventSlim(false);
+        }
+
+        public void LogMessage(string toLog)
+        {
+            var logger = _services.GetRequiredService<ILogger>();
+            logger.LogInformation(toLog);
+            ManualResetEvent.Set();
+        }
+    }
+}
