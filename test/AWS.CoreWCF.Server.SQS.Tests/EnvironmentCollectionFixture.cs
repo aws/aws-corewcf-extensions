bcbfe890cf68c1f3bd04ce1d6b680d16f3e28a4f
Jon Louie <louiejon@amazon.com>
2022-09-27 19:07:11 -0700

    Add initial integration test files

---
+++ test/AWS.CoreWCF.Server.SQS.Tests/EnvironmentCollectionFixture.cs
@@ -0,0 +1,54 @@
+﻿using Amazon.Runtime;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace AWS.CoreWCF.Server.SQS.Tests;
+
+public class EnvironmentCollectionFixture
+{
+    private const string AwsKey = "AWS";
+    private const string AccessKeyEnvVariable = "AWS_ACCESS_KEY_ID";
+    private const string SecretKeyEnvVariable = "AWS_SECRET_ACCESS_KEY";
+    private const string TestQueueUrlEnvVariable = "TEST_QUEUE_URL";
+    private const string SuccessTopicArnEnvVariable = "SUCCESS_TOPIC_ARN";
+    private const string FailureTopicArnEnvVariable = "FAILURE_TOPIC_ARN";
+
+    public static string? AccessKey { get; set; }
+    public static string? SecretKey { get; set; }
+    public static string? QueueUrl { get; set; }
+    public static string? SuccessTopicArn { get; set; }
+    public static string? FailureTopicArn { get; set; }
+
+    public EnvironmentCollectionFixture()
+    {
+        //using var file = File.OpenText();
+        var json = File.ReadAllText("appsettings.test.json");
+        //var reader = new JsonTextReader(file);
+        var appSettingsDictionary = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
+
+        var settingsDict = appSettingsDictionary[AwsKey];
+        AccessKey = settingsDict[AccessKeyEnvVariable];
+        SecretKey = settingsDict[SecretKeyEnvVariable];
+        QueueUrl = settingsDict[TestQueueUrlEnvVariable];
+        SuccessTopicArn = settingsDict[SuccessTopicArnEnvVariable];
+        FailureTopicArn = settingsDict[FailureTopicArnEnvVariable];
+    }
+
+    public static AWSCredentials GetCredentials()
+    {
+        return new BasicAWSCredentials(AccessKey, SecretKey);
+    }
+
+    public static string? GetTestQueueUrl()
+    {
+        return QueueUrl;
+    }
+}
+
+[CollectionDefinition("Database collection")]
+public class DatabaseCollection : ICollectionFixture<EnvironmentCollectionFixture>
+{
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the
+    // ICollectionFixture<> interfaces.
+}
---
+++ test/AWS.CoreWCF.Server.SQS.Tests/IntegrationTests.cs
@@ -0,0 +1,87 @@
+﻿using Amazon.Extensions.NETCore.Setup;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using AWS.CoreWCF.Server.SQS.Tests.TestService;
+using CoreWCF.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using CoreWCF.Channels;
+using CoreWCF.Queue.Common.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+using Xunit.Abstractions;
+using AWS.CoreWCF.Server.SQS.CoreWCF.DispatchCallbacks;
+
+namespace AWS.CoreWCF.Server.SQS.Tests;
+
+[Collection("Environment collection")]
+public class IntegrationTests : EnvironmentCollectionFixture
+{
+    private readonly ITestOutputHelper _output;
+
+    public IntegrationTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public async Task SomeTest()
+    {
+        using var host = ServiceHelper.CreateServiceHost<Startup>().Build();
+        await host.StartAsync();
+        //await MessageHelper.SendMessageToQueueAsync(nameof(ILoggingService), nameof(ILoggingService.LogMessage));
+
+        //var loggingService = host.Services.GetService<LoggingService>();
+        //Assert.True(loggingService.ManualResetEvent.Wait(TimeSpan.FromSeconds(5)));
+        await Task.Delay(5000);
+        Console.WriteLine("Waiting for newline character...");
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddScoped<LoggingService>();
+            services.AddServiceModelServices();
+            services.AddSingleton<ILogger>(NullLogger.Instance);
+#if DEBUG
+            services.AddLogging(builder =>
+            {
+                builder.AddConsole();
+            });
+#endif
+            services.AddDefaultAWSOptions(new AWSOptions
+            {
+                Credentials = GetCredentials()
+            });
+            services.AddAWSService<IAmazonSQS>();
+            services.AddAWSService<IAmazonSimpleNotificationService>();
+            services.AddQueueTransport();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var queueUrl = GetTestQueueUrl() ?? string.Empty;
+            var concurrencyLevel = 1;
+            var successTopicArn = SuccessTopicArn;
+            var failureTopicArn = FailureTopicArn;
+            app.UseServiceModel(services =>
+            {
+                services.AddService<LoggingService>();
+                services.AddServiceEndpoint<LoggingService, ILoggingService>(
+                    new AwsSqsBinding(
+                        queueUrl, 
+                        concurrencyLevel,
+                        new DispatchCallbacksCollection(
+                            successTopicArn,
+                            failureTopicArn,
+                            DispatchCallbacks.DefaultSuccessNotificationCallbackWithSns,
+                            DispatchCallbacks.DefaultFailureNotificationCallbackWithSns)
+                    ),
+                    //queueUrl);
+            "/BasicSqsService/ILoggingService.svc");
+        });
+        }
+    }
+}
