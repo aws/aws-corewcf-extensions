88279723001b6ab7a66dfa4099b3745a349dde5b
Jon Louie <louiejon@amazon.com>
2022-09-27 19:03:16 -0700

    add an extensions project

---
+++ AWS.CoreWCF.Server.Common/SdkClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Runtime;
+
+namespace AWS.CoreWCF.Server.Common;
+
+public static class SdkClientExtensions
+{
+    /// <summary>
+    /// Uses the response object to determine if the http request was successful.
+    /// </summary>
+    /// <param name="response">Response to validate with</param>
+    /// <exception cref="HttpRequestException">Thrown if http request was unsuccessful</exception>
+    public static void Validate(this AmazonWebServiceResponse response)
+    {
+        var statusCode = (int)response.HttpStatusCode;
+        if (statusCode < 200 || statusCode >= 300)
+        {
+            throw new HttpRequestException($"HttpStatusCode: {statusCode}");
+        }
+    }
+
+}
---
+++ AWS.CoreWCF.Server.Common/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace AWS.CoreWCF.Server.Common;
+
+public static class StringExtensions
+{
+    private static readonly Encoding DefaultEncoding = Encoding.UTF8;
+
+    /// <summary>
+    /// Converts a string to a Stream object using the default encoding
+    /// </summary>
+    /// <param name="str">String to convert to a stream</param>
+    /// <returns>A stream containing the string data encoded with default encoding</returns>
+    public static Stream ToStream(this string str)
+    {
+        return str.ToStream(DefaultEncoding);
+    }
+
+    /// <summary>
+    /// Converts a string to a Stream object using a specified encoding
+    /// </summary>
+    /// <param name="str">String to convert to a stream</param>
+    /// <param name="encoding">Encoding to apply to the string data</param>
+    /// <returns>A stream containing the string data encoded with specified encoding</returns>
+    public static Stream ToStream(this string str, Encoding encoding)
+    {
+        return new MemoryStream(encoding.GetBytes(str ?? ""));
+    }
+}
