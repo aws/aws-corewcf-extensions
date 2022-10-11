199c82f02ae5e6c098e3b891bf31143ca0285007
Jon Louie <louiejon@amazon.com>
2022-10-10 22:36:41 -0700

    reset branch

--- src/AWS.CoreWCF.Extensions/SQS/Channels/AwsSqsMessageContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using CoreWCF.Queue.Common;
-
-namespace AWS.CoreWCF.Extensions.SQS.Channels;
-
-public class AwsSqsMessageContext : QueueMessageContext
-{
-    public string? MessageReceiptHandle { get; set; }
-}
--- src/AWS.CoreWCF.Extensions/SQS/DispatchCallbacks/IDispatchCallbacksCollection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AWS.CoreWCF.Extensions.SQS.DispatchCallbacks;
-
-public interface IDispatchCallbacksCollection
-{
-    public NotificationDelegate NotificationDelegateForSuccessfulDispatch { get; set; }
-    public NotificationDelegate NotificationDelegateForFailedDispatch { get; set; }
-}
