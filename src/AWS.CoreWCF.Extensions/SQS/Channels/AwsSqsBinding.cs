384b231cb7d65c393eb35bbc5363bcf09277f579
philip pittle <pittle@amazon.com>
2023-07-19 08:54:52 -0700

    organize AwsSqsBinding class

--- src/AWS.CoreWCF.Extensions/SQS/Channels/AwsSqsBinding.cs
+++ src/AWS.CoreWCF.Extensions/SQS/Channels/AwsSqsBinding.cs
@@ -5,8 +5,39 @@ namespace AWS.CoreWCF.Extensions.SQS.Channels;
 
 public class AwsSqsBinding : Binding
 {
-    private readonly IDispatchCallbacksCollection _dispatchCallbacksCollection;
     public const int DefaultMaxMessageSize = 262144; // Max size for SQS message is 262144 (2^18)
+
+    /// <summary>
+    /// Creates an SQS binding
+    /// </summary>
+    /// <param name="queueName">Name of the queue</param>
+    /// <param name="concurrencyLevel">Maximum number of workers polling the queue for messages</param>
+    /// <param name="dispatchCallbacksCollection">Collection of callbacks to be called after message dispatch</param>
+    /// <param name="maxMessageSize">The maximum message size in bytes for messages in the queue</param>
+    public AwsSqsBinding(
+        string queueName,
+        int concurrencyLevel,
+        IDispatchCallbacksCollection dispatchCallbacksCollection,
+        int maxMessageSize = DefaultMaxMessageSize
+    )
+    {
+        QueueName = queueName;
+        ConcurrencyLevel = concurrencyLevel;
+        MaxMessageSize = maxMessageSize;
+        Name = nameof(AwsSqsBinding);
+        Namespace = "https://schemas.aws.sqs.com/2007/sqs/";
+
+        Transport = new AwsSqsTransportBindingElement(
+            QueueName,
+            ConcurrencyLevel,
+            dispatchCallbacksCollection,
+            MaxMessageSize
+        );
+        Encoding = new TextMessageEncodingBindingElement();
+        MaxMessageSize = DefaultMaxMessageSize;
+    }
+    
+    public override BindingElementCollection CreateBindingElements() => new() { Encoding, Transport };
 
     /// <summary>
     /// Gets the scheme used by the binding, https
@@ -38,45 +69,5 @@ public class AwsSqsBinding : Binding
     /// </summary>
     public AwsSqsTransportBindingElement? Transport { get; private set; }
 
-    /// <summary>
-    /// Creates an SQS binding
-    /// </summary>
-    /// <param name="queueName">Name of the queue</param>
-    /// <param name="concurrencyLevel">Maximum number of workers polling the queue for messages</param>
-    /// <param name="dispatchCallbacksCollection">Collection of callbacks to be called after message dispatch</param>
-    /// <param name="maxMessageSize">The maximum message size in bytes for messages in the queue</param>
-    public AwsSqsBinding(
-        string queueName,
-        int concurrencyLevel,
-        IDispatchCallbacksCollection dispatchCallbacksCollection,
-        int maxMessageSize = DefaultMaxMessageSize
-    )
-    {
-        QueueName = queueName;
-        ConcurrencyLevel = concurrencyLevel;
-        _dispatchCallbacksCollection = dispatchCallbacksCollection;
-        MaxMessageSize = maxMessageSize;
-        Name = nameof(AwsSqsBinding);
-        Namespace = "https://schemas.aws.sqs.com/2007/sqs/";
-
-        Initialize();
-    }
-
-    public override BindingElementCollection CreateBindingElements()
-    {
-        var elements = new BindingElementCollection { Encoding, Transport };
-        return elements;
-    }
-
-    private void Initialize()
-    {
-        Transport = new AwsSqsTransportBindingElement(
-            QueueName,
-            ConcurrencyLevel,
-            _dispatchCallbacksCollection,
-            MaxMessageSize
-        );
-        Encoding = new TextMessageEncodingBindingElement();
-        MaxMessageSize = DefaultMaxMessageSize;
-    }
+    
 }
